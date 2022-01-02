129f6dfdbf770101adb3e97aa628a4940d160f00
saheb mohammadi <saheb.put92@gmail.com>
2022-01-02 15:42:02 +0330

    add siteHeader

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app" class="min-h-screen bg-gray-200 antialiased">
+    <SiteHeader />
+    <section class="bg-gray-800 px-4 py-3 flex justify-between items-center">
+      <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+          </svg>
+        </div>
+        <input class="w-full pl-10 pr-3 py-2 rounded-lg outline-none bg-gray-900 text-white focus:outline-none" type="text" placeholder="Search by keywords" />
+      </div>
+
+      <button class="outline-none border-0 inline-flex p-2 rounded-lg px-3 bg-gray-700 hover:bg-gray-600">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+        </svg>
+        <!-- <span class="text-white ml-1 font-medium"> Filters</span> -->
+      </button>
+    </section>
+    <main class="px-4 py-6">
+      <h3 class="text-xl tex-gray-900">Los Angeles</h3>
+      <p class="text-gray-600">Live like the stars in these luxurious Southern California estates.</p>
+      <div class="mt-6">
+        <article class="">
+          <div>
+            <img class="rounded-lg h-64 object-cover w-full" src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+          </div>
+          <div class="relative -mt-16 px-4">
+            <div class="shadow-xl rounded-lg p-4 bg-white">
+              <span class="rounded-full bg-green-200 text-green-800 px-2 py-1 leading-none font-medium text-xs uppercase tracking-wide">Plus</span>
+              <span class="text-gray-400 font-semibold uppercase ml-3">3 beds &bull; 2 baths</span>
+              <h4 class="text-gray-900 mt-2 font-semibold text-base">Modern home in city center</h4>
+              <div class="mt-1">
+                <span class="text-gray-900">$1,400</span>
+                <span class="text-gray-600 ml-1 text-sm">/wk</span>
+              </div>
+              <div class="mt-2 text-sm text-gray-600 flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  />
+                </svg>
+                <span class="ml-2">40 reviews</span>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import SiteHeader from "./components/SiteHeader.vue";
+
+export default {
+  name: "app",
+  components: {
+    SiteHeader,
+  },
+};
+</script>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="bg-gray-900">
+    <div class="flex items-center justify-between text-white px-4 py-3">
+      <div class="text-gray-400 font-bold">DIGITIZE</div>
+      <div class="flex" :class="{ antialiased: isOpen }">
+        <button @click="toggle" class="px-2 text-gray-500 hover:text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path v-if="isOpen" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            <path v-else fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd" />
+          </svg>
+        </button>
+      </div>
+    </div>
+    <nav class="text-white" v-show="isOpen">
+      <div class="px-2 pt-1 pb-5 border-b border-gray-100">
+        <a href="#" class="block px-2 py-1 rounded font-semibold hover:bg-gray-800">List your property</a>
+        <a href="#" class="block px-2 py-1 rounded font-semibold mt-1 hover:bg-gray-800">Trips</a>
+        <a href="#" class="block px-2 py-1 rounded font-semibold mt-1 hover:bg-gray-800">Messages</a>
+      </div>
+      <div class="px-4 py-5">
+        <div class="flex items-center">
+          <img class="w-10 h-10 object-cover rounded-full border-gray-600 border-2" src="https://avatars.githubusercontent.com/u/60036248?v=4" alt="" />
+          <span class="ml-4 text-gray-200">Saheb Mohammadi</span>
+        </div>
+        <div class="mt-5">
+          <a href="#" class="block text-gray-400 hover:text-white">Account Settings</a>
+          <a href="#" class="mt-3 block text-gray-400 hover:text-white">Support</a>
+          <a href="#" class="mt-3 block text-gray-400 hover:text-white">Signout</a>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  props: [],
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggle() {
+      console.log("clicked");
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
